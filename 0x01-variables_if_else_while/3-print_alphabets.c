98aa24ada5c311664e043c1edcece692fa132965
oussamasedati <112280944+oussamasedati@users.noreply.github.com>
2023-03-18 01:01:20 +0100

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,16 +1,12 @@
-#include <stdio.h>
-
 int main(void)
 {
     char c;
 
     for (c = 'a'; c <= 'z'; c++)
         putchar(c);
-
     for (c = 'A'; c <= 'Z'; c++)
         putchar(c);
-
     putchar('\n');
 
-    return 0;
+    return (0);
 }
