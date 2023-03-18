0987da2026448646b38caaa65449efe4e4ad0f0e
oussamasedati <112280944+oussamasedati@users.noreply.github.com>
2023-03-18 01:30:49 +0100

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
-
+/**
+ * main - entry point
+ * Description: print alphabt
+ * Return: Always 0 (Success)
+ */
 int main() {
-  char letter;
-
-  for (letter = 'a'; letter <= 'z'; letter++) {
-    if (letter != 'q' && letter != 'e') {
-      putchar(letter);
-    }
-  }
-
-  putchar('\n');
-  
-  return 0;
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++) {
+if (letter != 'q' && letter != 'e') {
+putchar(letter);
+}
+}
+putchar('\n');
+return (0);
 }
