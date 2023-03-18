f2297fbe673e2aba288711fe2652ccef6f575bf6
oussamasedati <112280944+oussamasedati@users.noreply.github.com>
2023-03-18 01:41:51 +0100

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
-
-int main(void) {
-    int i;
-    for (i = 0; i < 10; i++) {
-        putchar(i + '0');
-    }
-    putchar('\n');
-    return 0;
+/**
+ * main - entry point
+ * Description: print numberz
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+putchar(i + '0');
+putchar('\n');
+return (0);
 }
