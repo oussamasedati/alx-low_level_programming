71f1eafabbdd43fc3b0d4d8495863da2cfcbbaf7
oussamasedati <112280944+oussamasedati@users.noreply.github.com>
2023-03-18 01:13:35 +0100

    Create 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int i, j;
+
+    for (i = 0; i < 10; i++)
+    {
+        for (j = i; j < 10; j++)
+        {
+            putchar(i + '0');
+            putchar(j + '0');
+            if (i != 9 || j != 9)
+            {
+                putchar(',');
+                putchar(' ');
+            }
+        }
+    }
+    putchar('\n');
+
+    return 0;
+}
