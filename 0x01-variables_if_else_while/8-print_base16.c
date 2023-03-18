8dd332f83580e9155d6954f92c2463c48a4769bc
oussamasedati <112280944+oussamasedati@users.noreply.github.com>
2023-03-18 01:10:01 +0100

    Create 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+  int i;
+  for(i = 0; i < 10; i++) {
+    putchar(i + '0');
+  }
+  for(i = 0; i < 6; i++) {
+    putchar(i + 'A');
+  }
+  putchar('\n');
+  return 0;
+}
