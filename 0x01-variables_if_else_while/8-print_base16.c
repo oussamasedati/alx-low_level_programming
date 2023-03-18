f9dd784edd94df88ff212e2c133bb19325ce41f6
oussamasedati <112280944+oussamasedati@users.noreply.github.com>
2023-03-18 01:11:26 +0100

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 
-int main(void) {
-  int i;
-  for(i = 0; i < 10; i++) {
-    putchar(i + '0');
-  }
-  for(i = 0; i < 6; i++) {
-    putchar(i + 'A');
-  }
-  putchar('\n');
-  return 0;
+int main()
+{
+    int i;
+
+    for (i = 0; i < 16; i++) {
+        if (i < 10) {
+            putchar(i + '0');
+        } else {
+            putchar(i - 10 + 'a');
+        }
+    }
+    putchar('\n');
+
+    return 0;
 }
