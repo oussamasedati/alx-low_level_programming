1a106fdc9c01b349226d80f24c08f147ffee9da5
oussamasedati <112280944+oussamasedati@users.noreply.github.com>
2023-03-18 01:15:34 +0100

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,21 +2,14 @@
 
 int main(void)
 {
-    int i, j;
-
-    for (i = 0; i < 10; i++)
+    int num;
+    for (num = 0; num < 10; num++)
     {
-        for (j = i + 1; j < 10; j++)
+        putchar(num + '0');
+        if (num < 9)
         {
-            putchar(i + '0');
             putchar(',');
             putchar(' ');
-            putchar(j + '0');
-            if (i != 8 || j != 9) // don't print comma and space after last pair
-            {
-                putchar(',');
-                putchar(' ');
-            }
         }
     }
     putchar('\n');
