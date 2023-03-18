e0b42abb834480e948e40e33994cf6e61021b119
oussamasedati <112280944+oussamasedati@users.noreply.github.com>
2023-03-18 01:39:36 +0100

    Update 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
-
-int main() {
-    int i;
-    for (i = 0; i < 10; i++) {
-        putchar(i + '0');
-    }
-    putchar('\n');
-    return 0;
+/**
+ * main - entry point
+ * Description: print numbers
+ * Return: Always 0 (Success)
+ */
+int main(void) {
+int i;
+for (i = 0; i < 10; i++)
+{
+putchar(i + '0');
+}
+putchar('\n');
+return (0);
 }
