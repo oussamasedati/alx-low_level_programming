3dfdd952a38902cade70fe7f747be842804cda51
oussamasedati <112280944+oussamasedati@users.noreply.github.com>
2023-03-18 01:34:55 +0100

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,9 +7,9 @@
 int main(void)
 {
 char letter;
-for(letter='a';letter<='z';letter++) 
+for (letter='a' ; letter<='z' ; letter++) 
 {
-if(letter!='q'&&letter!='e') 
+if (letter!='q' && letter!='e') 
 putchar(letter);
 }
 putchar('\n');
