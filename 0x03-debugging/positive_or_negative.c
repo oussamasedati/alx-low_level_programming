ba0e288c2f5365859dbfbefd10b19ec29c8bb378
oussamasedati <oussama.sedati@gmail.com>
2023-03-23 19:59:37 -0700

    sd

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,27 +1,10 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
-<<<<<<< HEAD
 #include "main.h"
-=======
->>>>>>> a6e81f90e5976b2e88887fbde24e7aab7de99df4
-/**
- * main - entry point
- * Description: Assigns a random number to the variable n and
- *              prints whether the number is positive or negative
- * Return: Always 0 (Success)
- */
-int main(void)
+void positive_or_negative(int i)
 {
-int n;
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-printf("%d ", n);
-if (n > 0)
-printf("is positive\n");
-else if (n == 0)
-printf("is zero\n");
+if (i>0)
+printf("%d is positive\n", i);
+else if (i<0)
+printf("%d is negative\n", i);
 else
-printf("is negative\n");
-return (0);
+printf("%d is zero\n", i);
 }
