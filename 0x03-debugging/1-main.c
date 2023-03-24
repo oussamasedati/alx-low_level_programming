1be49aa61d0883a8f65d7f8c90545df41e7ccd6a
oussamasedati <112280944+oussamasedati@users.noreply.github.com>
2023-03-24 03:07:30 +0000

    Update 1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,16 +1,18 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
+* main - causes an infinite loop
+* Return: 0
+*/
 int main(void)
 {
-char c;
-c = '0';
-printf("%c: %d\n", c, _isdigit(c));
-c = 'a';
-printf("%c: %d\n", c, _isdigit(c));
+int i;
+printf("Infinite loop incoming :(\n");
+i = 0;
+/*while (i < 10)*/
+/*{*/
+/*putchar(i);*/
+/*}*/
+printf("Infinite loop avoided! \\o/\n");
 return (0);
 }
