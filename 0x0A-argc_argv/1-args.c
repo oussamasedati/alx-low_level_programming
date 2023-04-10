dba3b356b3fc7a9e24fcaaa3c35a1276859f1d44
oussamasedati <112280944+oussamasedati@users.noreply.github.com>
2023-04-10 21:48:51 +0000

    0x0A-argc_argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * @argc: int
+ * @argv: vector
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%i\n", argc - 1);
+	return (0);
+}
